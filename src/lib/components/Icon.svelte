67b9c51f80f079c5a4bdd152ccd4497a44b4430f
Kimon Meier <kimon.meier@gmail.com>
2026-08-29 03:29:30 +0200

    Add deprecation warning for svelte component in Icon.svelte

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -12,4 +12,5 @@
 	let { icon, size = 24, strokeWidth = 2, class: className = '', ...rest }: Props = $props();
 </script>
 
+<!-- svelte-ignore svelte_component_deprecated -->
 <svelte:component this={icon} {size} {strokeWidth} class={className} {...rest} />
